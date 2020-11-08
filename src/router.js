b1fcd2d011a2f0b1d1e4d85b1971557273c8af9b
orione_90@hotmail.com <orione_90@hotmail.com>
2020-11-08 10:36:03 +0100

    load routes async

--- src/router.js
+++ src/router.js
@@ -1,17 +1,36 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from './components/Home/AppHomePage.vue'
 import Achievements from './components/Achievements/AppAchievements.vue'
 import Contact from './components/contact/appContact.vue'
 import About from './components/About/AboutMe.vue'
-import RestaurantReview from './components/Achievements/Projects/Restaurant_review.vue'
-import CatFacts from './components/Achievements/Projects/Cat_facts.vue'
-import NinjaFight from './components/Achievements/Projects/Ninja_fight.vue'
-import FilmFestival from './components/Achievements/Projects/Film_festival.vue'
-import DiceGame from './components/Achievements/Projects/Dice_game.vue'
-import QuoteGenerator from './components/Achievements/Projects/Quote_generator.vue'
-import TodoTesting from './components/Achievements/Projects/Todo_testing.vue'
-import ExpressFood from './components/Achievements/Projects/Express_food.vue'
+
+// import RestaurantReview from './components/Achievements/Projects/Restaurant_review.vue'
+// import CatFacts from './components/Achievements/Projects/Cat_facts.vue'
+// import NinjaFight from './components/Achievements/Projects/Ninja_fight.vue'
+// import FilmFestival from './components/Achievements/Projects/Film_festival.vue'
+// import DiceGame from './components/Achievements/Projects/Dice_game.vue'
+// import QuoteGenerator from './components/Achievements/Projects/Quote_generator.vue'
+// import TodoTesting from './components/Achievements/Projects/Todo_testing.vue'
+// import ExpressFood from './components/Achievements/Projects/Express_food.vue'
+
+const RestaurantReview = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Restaurant_review.vue'))
+const CatFacts = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Cat_facts.vue'))
+const NinjaFight = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Ninja_fight.vue'))
+const FilmFestival = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Film_festival.vue'))
+const DiceGame = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Dice_game.vue'))
+const QuoteGenerator = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Quote_generator.vue'))
+const TodoTesting = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Todo_testing.vue'))
+const ExpressFood = defineAsyncComponent(() =>
+    import ('./components/Achievements/Projects/Express_food.vue'))
 
 
 const router = createRouter({
